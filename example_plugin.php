d22feb163123667c64156d6ce8c2b452e7aa2ca7
Richard <richlloydmiles@gmail.com>
2016-04-12 14:50:48 +0200

    Update example_plugin.php

--- example_plugin.php
+++ example_plugin.php
@@ -6,6 +6,15 @@
 ?>
 
 <?php 
+############################
+# GET POST CONTENT FROM ID
+############################
+$my_postid = 9120;//This is page id or post id
+$content_post = get_post($my_postid);
+$content = $content_post->post_content;
+$content = apply_filters('the_content', $content);
+$content = str_replace(']]>', ']]&gt;', $content);
+echo  $content_post->post_title;
 
 ############################
 # CMB DECLAREATION
