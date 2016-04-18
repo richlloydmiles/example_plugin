c725ccaa366d0fe6723919afd230c4c8e6ce7c86
Richard <richlloydmiles@gmail.com>
2016-04-18 15:03:12 +0200

    Update example_plugin.php

--- example_plugin.php
+++ example_plugin.php
@@ -364,4 +364,20 @@ return array(
         ),
     'orderby'     => array( 'meta_value' => 'DESC' ),
     );
-?>
+    
+###########################
+//Pagination
+###########################
+
+		
+				global $wp_query;
+
+				$big = 999999999; 
+				echo paginate_links( array(
+					'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+					'format' => '?paged=%#%',
+					'current' => max( 1, get_query_var('paged') ),
+					'total' => $wp_query->max_num_pages
+					) );
+
+					?>
